9af1bd6bce600ed102aa67f6d42caacb0e81a513
tkmaher <tkmaher@umich.edu>
2025-11-20 16:57:41 -0600

    final for today

--- src/components/transitions.tsx
+++ src/components/transitions.tsx
@@ -3,7 +3,7 @@ export function fadeOut (id: string) {
     const item = document.getElementById(id);
     if (item) {
         item.style.opacity = "0";
-        item.style.transform = "translate(0%, 50em)";
+        //item.style.transform = "translate(0%, 50em)";
     }
 }
 
@@ -12,7 +12,7 @@ export function fadeIn(id: string, time: number) {
     if (item) {
         setTimeout(() => {
         item.style.opacity = "1";
-        item.style.transform = "translate(0%, 0%)";
+        //item.style.transform = "translate(0%, 0%)";
         }, time);
     }
 }
